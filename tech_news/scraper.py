de781f0bc38637a3ec2339bc40cb402d69db624c
Gustavo Vono <guvono4@gmail.com>
2023-03-13 23:17:54 -0300

    Req 3 ok - 4 incompleto

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -26,13 +26,26 @@ def scrape_updates(html_content: str) -> List[str]:
 
 
 # Requisito 3
-def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+def scrape_next_page_link(html_content: str) -> str or None:
+    page_content = Selector(html_content)
+    if page_content:
+        return page_content.css('a.next.page-numbers::attr(href)').get()
+    return None
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    html = Selector(html_content)
+
+    return {
+        "url": html.css('a.cs-overlay-link::attr(href)').get(),
+        "title": html.css('h1.entry-title::text').get(),
+        "timestamp": html.css('li.meta-date::text').get(),
+        "writer": html.css('span.author a::text').get(),
+        "reading_time": int(html.css('li.meta-reading-time i::text').get()[0]),
+        "summary": html.css('div.entry-content p *::text').get(),
+        "category": html.css('span.label::text').get()
+    }
 
 
 # Requisito 5
